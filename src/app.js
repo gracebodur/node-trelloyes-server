bb3cb515734b457cabc1fb8b370fa1bbc22993b2
Grace <mgracebodur@gmail.com>
2019-12-26 23:59:43 -0500

    Updated READ.md file

--- src/app.js
+++ src/app.js
@@ -54,4 +54,8 @@ app.use(function errorHandler(error, req, res, next) {
     res.status(500).json(response)
 })
 
+app.get('/', (req, res) => {
+    res.send("Hello, world!")
+  })
+
 module.exports = app
